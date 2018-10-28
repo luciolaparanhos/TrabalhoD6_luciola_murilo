ba38d5f8c194193a3bdbe2c25e44f3b75ae0813f
luciolaparanhos <luciolaparanhos@yahoo.com.br>
2018-10-28 18:55:33 -0300

    Inserção de explicações da pesquisa e novos gráficos

--- bases trabalho upa.R
+++ bases trabalho upa.R
@@ -12,32 +12,66 @@ lapply(lista.de.pacotes, require, character.only=T)
 rm(lista.de.pacotes,novos.pacotes)
 gc()
 
-#importando bases UPA E PRODU??O SIA/SUS
+#importando bases UPA E PRODUÇÃO SIA/SUS
+# controleupa - Dados administrativos da Coordenação de Urgência do Ministério da Saúde
+# registra as propostas de construção de UPA
+# producaoupa - Extração com Tabwin do Sistema SIA/SUS do Ministério da Saúde
+# registra a produção das UPA que estão em funcionamento
 controleupa <- read_xlsx("C:/Users/Luciola/Documents/Pos ENAP ADPP/D6analise/trabalho final/controle_upa24h_ago2018.xlsx", skip = 0)
 producaoupa <- read_xlsx("C:/Users/Luciola/Documents/Pos ENAP ADPP/D6analise/trabalho final/producao_upa_13082018.xlsx", skip = 3)
 
 controleupa <- controleupa [,c(1,2,5,6,7,8,10,11,13,17,24,25,37,38,40,41,42,43)]
 
 
-
-
-
+# resumindo opÇÕES de status
 controleupa <- controleupa %>%
-# resumindo op??es de status
-  mutate(situacao = str_sub(Status_UPA, 1,2))
+  mutate(situacao = str_sub(Status_UPA, 1,2),
+         situacao = case_when(
+           situacao == "1."~"Ação preparatória",
+           situacao == "2."~"Ação preparatória",
+           situacao == "3."~"Em obras",
+           situacao == "4 "~"Concluída",
+           situacao == "4."~"Em funcionamento")
+  )
+
+# total e percentual de upa por situação e por UF
+UPA_UF_situacao <- controleupa %>%
+  group_by(UF,situacao) %>%
+  summarise(total= n())%>%
+  mutate(percent=prop.table(total))
+
+
+# total e percentual de upa por ano da proposta e por situação
+UPA_progr_situa <- controleupa %>%
+  filter(!progr== "SES/SMS")%>%
+  group_by(progr,situacao) %>%
+    summarise(total= n())%>%
+    mutate(percent=prop.table(total))
+
+# GRAFICO UPA por situação e ano da proposta
+UPA_progr <- ggplot(data=UPA_progr_situa, aes(x=progr, y=total, fill=situacao)) +
+  geom_bar(stat="identity")+
+  ggtitle("TOTAL DE UPA POR ANO PROPOSTA E SITUAÇÃO DE IMPLANTAÇÃO")+
+  labs(x="Ano proposta", y = "Nº de UPA")
+UPA_progr
+
+
+# GRAFICO UPA por situação e UF
+situacao_estado <- ggplot(data=UPA_UF_situacao, aes(x=UF, y=total, fill=situacao)) +
+  geom_bar(stat="identity")+
+  ggtitle("TOTAL DE UPA POR UF E SITUAÇÃO DE IMPLANTAÇÃO")+
+  labs(x="UF", y = "Nº de UPA")
+situacao_estado
 
 # TOTALiza  UPA por situação (status resumido)
 Controle_status <- controleupa %>%
   group_by(situacao)%>%
     summarise(nr_UPA = n())%>%
-    mutate(percent=prop.table(nr_UPA),
-           situacao = case_when(
-             situacao == "1."~"Ação preparatória",
-             situacao == "2."~"Ação preparatória",
-             situacao == "3."~"Em obras",
-             situacao == "4 "~"Concluída",
-             situacao == "4."~"Em funcionamento")
-           )
+    mutate(percent=round(prop.table(nr_UPA)*100,1) )
+
+#Controle_status
+
+View(Controle_status)
 
 # total e percentual de upa por porte e por UF
 UPA_UFporte <- controleupa %>%
@@ -74,7 +108,6 @@ upa_porte_func <- controleupa %>%
 
 
 install.packages('ggplot2')
-# Loading
 library(ggplot2)
 library(scales)
 
@@ -88,17 +121,11 @@ blank_theme <- theme_minimal()+
     plot.title=element_text(size=14, face="bold", hjust = 0.5)
   )
 #grafico upa por porte
-  barras_porte <- ggplot(upa_porte,aes(x="", y=total, fill=porte))+
-  geom_bar(width = 1, stat = "identity")
-  pizza_porte <- barras_porte + coord_polar("y", start=0)+
-  theme(axis.text.x=element_blank())+
-  geom_text(aes(y = total/3 + c(0, cumsum(total)[-length(total)]), 
-                  label = percent(total/sum(total))), size=4)+
-  ggtitle("% DE UPA POR PORTE")
+  barras_porte <-ggplot(upa_porte, aes(x = porte, y = total, fill = porte)) + geom_bar(stat = "identity")+
+  ggtitle("TOTAL DE UPA POR PORTE")
+  barras_porte
   
-pizza_porte + blank_theme + theme(axis.text.x=element_blank())
-
- # GRAFICO UPA EM FUNCIONAMENTO por porte e UF
+   # GRAFICO UPA EM FUNCIONAMENTO por porte e UF
  barras_porte_f <- ggplot(upa_porte_func,aes(x="", y=total, fill=porte))+
   geom_bar(width = 1, stat = "identity")
  pizza_porte_func <- barras_porte_f + coord_polar("y", start=0)+
@@ -106,7 +133,7 @@ pizza_porte + blank_theme + theme(axis.text.x=element_blank())
    geom_text(aes(y = total/3 + c(0, cumsum(total)[-length(total)]), 
                  label = percent(total/sum(total))), size=4)+
     ggtitle("% DE UPA EM FUNCIONAMENTO POR PORTE")
- pizza_porte_func + blank_theme + + theme(axis.text.x=element_blank())
+ pizza_porte_func + blank_theme + theme(axis.text.x=element_blank())
  
  # GRAFICO UPA por porte e UF
  porte_estado <- ggplot(data=UPA_UFporte, aes(x=UF, y=total, fill=porte)) +
@@ -116,25 +143,23 @@ pizza_porte + blank_theme + theme(axis.text.x=element_blank())
  # GRAFICO UPA EM FUNCIONAMENTO por porte e UF
  porte_estado_func <- ggplot(data=UPA_UFporte_func, aes(x=UF, y=total, fill=porte)) +
     geom_bar(stat="identity")+
-    ggtitle("TOTAL DE UPA EM FUNCIONAMENTO POR UF E POR PORTE")
-    #plot.title=element_text(size=14, face="bold", hjust = 0.5)
- porte_estado_func
+    ggtitle("TOTAL DE UPA EM FUNCIONAMENTO POR UF E POR PORTE")+
+  labs(x="", y = "Nº de UPA")
+    porte_estado_func
  
 library(lubridate)
  
-# datas que marcam evolu??o da implanta??o das upa, financiadas pelo Ministerio da Sa?de
-# exclu?das as constru?das por gestores locais e/ou realizadas pela Caixa Economica Federal
+# datas que marcam evolução da implantação das upa, financiadas pelo Ministerio da Saúde
+# excluídas as construídas por gestores locais e/ou realizadas pela Caixa Economica Federal
 evolucao_upa <- controleupa %>%
 filter(progr!="2008 CEF",!is.na(nrproposta))%>%
 select(nrproposta,UF,municipio, ano_habil_construcao,Data1aParcela,Data_OIS,data_conclusao,Ano_funcionamento)
 
-#acrescenta coluna que mostra tempo gasto na execu??o da obra, em dias
-  # a ideia ? tirar m?dia desse tempo por estado depois
+#acrescenta coluna que mostra tempo gasto na execução da obra, em dias
+# a ideia é tirar média desse tempo por estado depois
 evolucao_upa <- evolucao_upa %>%
 mutate(Ano_funcionamento = as.character(Ano_funcionamento))%>%
 mutate(tempo_obra = ymd(as.Date(data_conclusao)) - ymd(as.Date(Data_OIS))) #%>%
-#mutate(tempo_funciona = as.Date(Ano_funcionamento) - as.Date(data_conclusao))
-#mutate(Ano_funcionamento = c(Ano_funcionamento, "-07-01"))
 
 # mostra tempo média de obra por estado
 evolucao_upa_uf <- evolucao_upa%>%
@@ -145,10 +170,8 @@ evolucao_upa_uf <- evolucao_upa%>%
             min_dias_obra = min(tempo_obra),
             max_dias_obra = max(tempo_obra))
 
-#evolucao_upa [evolucao_upa$UF == "AL", c("UF", "municipio", "tempo_obra")]
-
   
-# AN?LISE DE PRODU??O DAS UPA
+# ANáLISE DE PRODUçãO DAS UPA
 
 # desempilhando dados de produção mensal das UPA, todos os procedimentos
 producao_mes <- producaoupa %>%
@@ -163,12 +186,10 @@ semproducao <- upa_funcionamento%>%
   anti_join(producao_mes)
 head(semproducao)
 
-#identifiquei 2 UPA que n?o estava indicadas como 'em funcionamento' mas que tinham produ??o
+#identifiquei 2 UPA que não estava indicadas como 'em funcionamento' mas que tinham produ??o
 erro <- producao_mes%>%
   anti_join(upa_funcionamento)
 
-
-# '0301060118' classificação de risco
 
 #Filtra só os procedimentos de atendimento médico
 atendimentos <- producaoupa %>%
@@ -188,20 +209,8 @@ atendimentos_mes <- atendimentos %>%
 atendimentos_mes2 <- atendimentos %>%
   mutate(CNES = as.numeric(CNES)) %>%
   group_by(CNES,UF,MUNIC,OPCAO_CUSTEIO)%>%
-  summarise(media_cnes_sem = round(sum(ATENDIMENTO)/6,0) #,
-            #prod_III = case_when(
-                   #OPCAO_CUSTEIO=="III"&media_cnes_sem>=4500~"DE ACORDO",
-                  #OPCAO_CUSTEIO=="V"&media_cnes_sem>=6750~"DE ACORDO",
-                  #OPCAO_CUSTEIO=="III"&media_cnes_sem>=10125~"DE ACORDO")
-                                )
- # mg <- atendimentos_mes2 %>%
-  #  select(CNES,OPCAO_CUSTEIO, media_cnes_sem) %>%
-   # filter(UF == 'MG')%>%
-  #box_atend_uf <- ggplot(mg, aes(x = CNES, y = media_cnes_sem))+
-  #geom_boxplot(aes(fill = OPCAO_CUSTEIO))
-#box_atend_uf
+  summarise(media_cnes_sem = round(sum(ATENDIMENTO)/6,0) )
  
-
   
 # mostra média de atendimentos de cada estado por PORTE no semestre
 # FILTRA Somente as opções III, V e VIII de custeio, que equivalem aos portes I, II e III
@@ -217,8 +226,6 @@ atendimentos_mes_uf2 <- atendimentos_mes2 %>%
                    V = sum(V),
                VIII = sum(VIII))
 
-  
-summary(atendimentos_mes_uf)
 
 # grafico boxplot da medIa atendimento por uf e porte I
 atend_porteI <- atendimentos_mes2 %>%
@@ -227,7 +234,8 @@ atend_porteI <- atendimentos_mes2 %>%
   filter(media_cnes_sem > 100)      #outlier (5 casos)
   box_atend_uf1 <- ggplot(atend_porteI, aes(x = UF, y = media_cnes_sem, fill=UF))+
   geom_boxplot()+  theme(legend.position="none")+
-    labs(title="Média mensal de atendimentos - UPA PORTE I por UF",x="", y = "Média Atendimentos/mês")
+    labs(title="Média mensal de atendimentos por UF - UPA PORTE I",x="", y = "Média Atendimentos/mês")+
+    geom_hline(yintercept = 4500, linetype="dashed", color = "red")
   box_atend_uf1
 
 # grafico boxplot da medIa atendimento por uf e porte II
@@ -237,8 +245,9 @@ atend_porteI <- atendimentos_mes2 %>%
     filter(media_cnes_sem > 350)      #outlier (2 casos)
     box_atend_uf2 <- ggplot(atend_porteII, aes(x = UF, y = media_cnes_sem,fill=UF))+
     geom_boxplot()+ theme(legend.position="none")+
-      labs(title="Média mensal de atendimentos - UPA PORTE II por UF",x="", y = "Média Atendimentos/mês")
-  box_atend_uf2
+      labs(title="Média mensal de atendimentos por UF - UPA PORTE II",x="", y = "Média Atendimentos/mês")+
+      geom_hline(yintercept = 6750, linetype="dashed", color = "red")
+    box_atend_uf2
   
 
 # grafico boxplot da medIa atendimento por uf e porte III
@@ -248,12 +257,9 @@ atend_porteI <- atendimentos_mes2 %>%
     filter(media_cnes_sem > 600)          #outlier (3 casos)
     box_atend_uf3 <- ggplot(atend_porteIII, aes(x = UF, y = media_cnes_sem, fill=UF))+
     geom_boxplot()+ theme(legend.position="none")+
-     labs(title="Média mensal de atendimentos-UPA PORTE III por UF",x="", y = "Média Atendimentos/mês")+
+     labs(title="Média mensal de atendimentos por UF - UPA PORTE III",x="", y = "Média Atendimentos/mês")+
+      geom_hline(yintercept = 10125, linetype="dashed", color = "red")
     box_atend_uf3
   
-    ?labs()
-  
-    #rowwise() %>%
-  #mutate(media = mean(c(var1, var2, var3), na.rm=T)) %>%
-  
+
 
